90ca739a3d5151a2a2465f94648735a5580a8fa4
MestreTopor <mestretopor666@gmail.com>
2020-09-22 13:27:00 +0300

    Home lesson #12

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 "use strict";
 
-const a = 1;
-const b = 'dfgh';
-console.log(b && a);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+
+const personalMovie = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+};
+
+const film = prompt("Один из последних просмотренных фильмов?","");
+const ocenka = prompt("Насколько оцените его,","");
+
+personalMovie.movies[film] = ocenka;
+
+console.log(personalMovie);
