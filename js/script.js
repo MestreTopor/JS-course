0d77cb7f86b512d16b8135c1432209063455cb36
MestreTopor <mestretopor666@gmail.com>
2020-09-23 03:41:03 +0300

    Home work #15

--- js/script.js
+++ js/script.js
@@ -10,9 +10,46 @@ const personalMovie = {
   private: false
 };
 
-const film = prompt("Один из последних просмотренных фильмов?","");
-const ocenka = prompt("Насколько оцените его,","");
+// let i = 0;
+// while (i <= 1) {
+//   let film = prompt("Один из последних просмотренных фильмов?","");
+//   let ocenka = prompt("Насколько оцените его,","");
 
-personalMovie.movies[film] = ocenka;
+//   if (film == null || film.length > 50 || film.length <= 0) {
+//     continue;
+//   } else {
+//     personalMovie.movies[film] = ocenka;
+//     i++;
+//   }
+// }
+
+for (let i = 0; i <= 1; i++) {
+  let film = prompt("Один из последних просмотренных фильмов?","");
+  let ocenka = prompt("Насколько оцените его,","");
+
+  if (film == null || film.length > 50 || film.length <= 0) {
+    i--;
+  } else {
+    personalMovie.movies[film] = ocenka;
+  }
+}
+
+if (personalMovie.count < 10) {
+
+  alert("You watched not a lot movies");
+
+} else if (personalMovie.count <= 10 && personalMovie.count >= 30) {
+
+  alert("You are the classic viewer");
+
+} else if (personalMovie.count > 30) {
+
+  alert("You are movie-man!");
+
+} else {
+  alert("Failed!");
+}
 
 console.log(personalMovie);
+
+console.log(personalMovie.count);
